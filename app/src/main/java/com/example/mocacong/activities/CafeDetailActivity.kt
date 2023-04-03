c4b120e4fa8201ebfc3e5b49e365ebb7ff379c53
ddiyooong <jsshin000107@naver.com>
2023-04-03 22:34:51 +0900

    feat: 카페 세부정보 Activity Layout 구현
    - res/drawable 경로에 아이콘 벡터 파일 추가
    -

---
+++ app/src/main/java/com/example/mocacong/activities/CafeDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mocacong.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mocacong.R
+
+class CafeDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cafe_detail)
+    }
+}
